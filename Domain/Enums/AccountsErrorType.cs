cf3759874429c613cb4d0770603e280d1b2fbe9d
tashievemir <tashievemir01@gmail.com>
2025-02-12 16:52:59 +0600

    added enum for errors

---
+++ Domain/Enums/AccountsErrorType.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum AccountsErrorType
+    {
+        [DifferentLanguageDescriptionAttribute("Email or password is incorrect", "Неверный email или пароль", "Электрондук почта же пароль туура эмес")]
+        IncorrectPasswordOrEmail = 0,
+
+        [DifferentLanguageDescriptionAttribute("Permission denied", "Доступ запрещен", "Рұқсат жоқ")]
+        PermissionDenied = 1,
+
+        [DifferentLanguageDescriptionAttribute("Invalid token", "Недействительный токен", "Жарамсыз токен")]
+        InvalidToken = 2
+    }
+}
---
+++ Domain/Helpers/DifferentLanguageDescriptionAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Helpers
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    sealed class DifferentLanguageDescriptionAttribute : Attribute
+    {
+        public string En { get; }
+        public string Ru { get; }
+        public string Kg { get; }
+
+        public DifferentLanguageDescriptionAttribute(string en, string ru, string kg)
+        {
+            En = en;
+            Ru = ru;
+            Kg = kg;
+        }
+    }
+}
