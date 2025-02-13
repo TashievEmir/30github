48b7e38ed4f39c616734c7b0cb4281679169a6c8
tashievemir <tashievemir01@gmail.com>
2025-02-13 21:37:06 +0600

    added pageResponse class

---
+++ Application/Models/PageResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Models
+{
+    public class PageResponse<T>
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public long TotalPages { get; set; }
+        public long TotalCount { get; set; }
+        public List<T> Content { get; set; }
+    }
+}
