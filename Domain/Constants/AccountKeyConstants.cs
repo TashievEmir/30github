a24083e3398aa365c56bc372962d53c122d926ea
tashievemir <tashievemir01@gmail.com>
2025-02-14 16:37:48 +0600

    added AccountKeyConstants class

---
+++ Domain/Constants/AccountKeyConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Constants
+{
+    public class AccountKeyConstants
+    {
+        public const string Authorization = "Authorization";
+        public const string RefreshToken = "refreshToken";
+        public const string XFF = "X-Forwarded-For";
+        public const string Admin = "admin";
+        public const string CurrentUser = "Account";
+        public const string Id = "id";
+        public const string PrevEntity = "EntityPrev";
+        public const string NotFound = "Not found";
+
+    }
+}
