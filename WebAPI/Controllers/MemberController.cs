0a52ff458d1ffbca70b8afcb9e423f3be7225b05
Emir Tashiev <tashievemir01@gmail.com>
2025-02-25 13:33:51 +0600

    changed inheritance of controller and added versioning to path of controller

--- WebAPI/Controllers/MemberController.cs
+++ WebAPI/Controllers/MemberController.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
-    public class MemberController : ControllerBase
+    public class MemberController : BaseController
     {
     }
 }
